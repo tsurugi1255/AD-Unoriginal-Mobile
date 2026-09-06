f14fd9560ec2eece8e243d8f066d67ca4e233a0d
tsurugi1255 <seahbryan4025@gmail.com>
2026-09-07 00:12:41 +0800

    update dimensions

--- src/components/tabs/antimatter-dimensions/ModernAntimatterDimensionsTab.vue
+++ src/components/tabs/antimatter-dimensions/ModernAntimatterDimensionsTab.vue
@@ -117,17 +117,17 @@ export default {
         :tier="tier"
       />
     </div>
-    <div class="resets-container">
-      <DimensionBoostRow />
-      <PrimaryButton
+    <PrimaryButton
         v-if="isQuickResetAvailable"
         class="o-primary-btn--quick-reset"
         onclick="softReset(-1, true, true)"
       >
         Perform a Dimension Boost reset
-        <span v-if="hasDimensionBoosts"> but lose a Dimension Boost</span>
+        <span v-if="hasDimensionBoosts"> but lose a Di>
         <span v-else> for no gain</span>
       </PrimaryButton>
+    <div class="resets-container">
+      <DimensionBoostRow />
       <AntimatterGalaxyRow />
     </div>
     <AntimatterDimensionProgressBar />
