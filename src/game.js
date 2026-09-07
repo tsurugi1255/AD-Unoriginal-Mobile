d7d916f09991751f14f58f5d8abd79142159d51e
tsurugi1255 <seahbryan4025@gmail.com>
2026-09-07 18:40:29 +0800

    update break infinity

--- src/game.js
+++ src/game.js
@@ -79,8 +79,6 @@ export function breakInfinity() {
   for (const autobuyer of Autobuyers.all) {
     if (autobuyer.data.interval !== undefined) autobuyer.maxIntervalForFree();
   }
-  // There's a potential migration edge case involving already-maxed autobuyers; this should give the achievement
-  Achievement(61).tryUnlock();
   Achievement(52).unlock();
   Achievement(43).unlock();
   Achievement(79).unlock();
@@ -88,6 +86,8 @@ export function breakInfinity() {
   Achievement(124).unlock();
   Achievement(134).unlock();
   Achievement(143).unlock();
+  // There's a potential migration edge case involving already-maxed autobuyers; this should give the achievement
+  Achievement(61).tryUnlock();
   player.break = !player.break;
   TabNotification.ICUnlock.tryTrigger();
   EventHub.dispatch(player.break ? GAME_EVENT.BREAK_INFINITY : GAME_EVENT.FIX_INFINITY);
