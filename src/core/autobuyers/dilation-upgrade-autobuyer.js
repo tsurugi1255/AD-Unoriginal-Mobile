87840511a11b7544ce1c3dbb6292d87e7a179e29
tsurugi1255 <seahbryan4025@gmail.com>
2026-09-08 21:38:58 +0800

    update dilation autobuyer interval

--- src/core/autobuyers/dilation-upgrade-autobuyer.js
+++ src/core/autobuyers/dilation-upgrade-autobuyer.js
@@ -12,7 +12,7 @@ export class DilationUpgradeAutobuyerState extends IntervaledAutobuyerState {
   }
 
   get interval() {
-    return 1000 * Perk.autobuyerFasterDilation.effectOrDefault(1) / PerkShopUpgrade.autoSpeed.effectOrDefault(1);
+        return 1000 * Perk.autobuyerFasterDilation.effectOrDefault(1) / Math.max(PerkShopUpgrade.autoSpeed.effectOrDefault(1), 4);
   }
 
   get isUnlocked() {
